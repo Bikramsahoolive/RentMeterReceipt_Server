3bac9d08b43bd3f6c0983e90b0726ef6c28c9f26
Bikram Sahoo <bikramsahoo@live.in>
2024-02-02 15:34:26 +0530

    Handel Wildcard routs and update logout module.

--- index.js
+++ index.js
@@ -11,13 +11,14 @@ const cors = require('cors');
 const session = require('express-session');// (stateful) or jwt auth (stateless).
 const cookieParser = require('cookie-parser');
 
-const SignupLandlordRouter = require('./src/routes/signUpLandlordRouts')
+const SignupLandlordRouter = require('./src/routes/signUpLandlordRouts');
 const adminRouter = require('./src/routes/adminRouter');
 const landlordRouter = require('./src/routes/landlordRouts');
 const rentHolderRouter = require('./src/routes/rentHolderRouts');
 const mainMeterRouter = require('./src/routes/mainMeterRouts');
 const rentBillRouter = require('./src/routes/rentBillRouts')
 const{checkRouter, landlordLogout} = require('./src/controller/authControl');
+const{checkSession}= require('./src/middlewares/session')
 
 const apiKeyValidation = require('./src/middlewares/validateApi');
 
@@ -58,8 +59,7 @@ app.use(express.json());
 app.use(express.urlencoded({extended:true}));
 
 //Handle Routes 
-app.get('/',(req,res)=>res.send("Welcome to Rent N meter.Receipt!"));
-// app.get('/**',(req,res)=>res.status('404').send('Rent n meter can not find any Route, Error code 404!'))
+app.get('/',(req,res)=>res.send("Welcome to Rentⓝmeter.Receipt!"));
 app.use('/signup',SignupLandlordRouter);
 app.use('/admin',adminRouter);
 app.use('/landlord',landlordRouter);
@@ -69,9 +69,12 @@ app.use('/mainMeter',mainMeterRouter);
 app.use('/rent-bill',rentBillRouter);
 
 app.post('/check-session',checkRouter);
-app.post('/logout',landlordLogout);
+app.post('/logout',checkSession,landlordLogout);
 
-
+// Handle Wild Card Routs.
+app.use((req,res,next)=>{
+  res.status('404').send('Rentⓝmeter.Receipt can not find this Route, Error code 404!');
+})
 
 
 
