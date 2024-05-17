d4ddaaad3dfc657d76828aae228ece76ffa0853f
Bikram Sahoo <bikramsahoo@live.in>
2024-05-17 16:02:53 +0530

    Uninstall dev dependencies.

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const {createServer} = require('http');
 
 //dev module
 
-const morgan = require('morgan');
+// const morgan = require('morgan');
 const cors = require('cors');
 
 
@@ -53,7 +53,7 @@ app.use(session({
     }
   }));
 
-app.use(morgan('dev'));
+// app.use(morgan('dev'));
 
 app.use(express.json({limit:'10mb'})); 
                    
